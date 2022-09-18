548cee795276b0e9a23b3a48bb707b2929ce0c36
Natalia <n.botygina@mail.ru>
2022-09-18 08:45:57 +0300

    frontend

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,6 +4,7 @@ function AboutProject() {
   return (
     <section className="aboutProject" id="aboutProject">
       <h2 className="aboutProject__title">О проекте</h2>
+      <div className="aboutProject__mainbox">
       <div className="aboutProject__box">
         <h3 className="aboutProject__boxtitle">
           Дипломный проект включал 5 этапов
@@ -21,6 +22,7 @@ function AboutProject() {
           У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
           соблюдать, чтобы успешно защититься.
         </p>
+      </div>
       </div>
       <div className="aboutProject__timeline">
         <div className="aboutProject__timeline_backend">
