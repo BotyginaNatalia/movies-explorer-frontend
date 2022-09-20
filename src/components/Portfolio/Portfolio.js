e61437b44aea00c79b15ff03494fc259924e532a
Natalia <n.botygina@mail.ru>
2022-09-20 12:04:16 +0300

    changes after 2d review

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,31 +5,40 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <div className="portfolio__box">
-        <nav className="portfolio__nav">
-          <a
-            href="https://botyginanatalia.github.io/how-to-learn/"
-            className="portfolio__nav_link" target="_blank"
-          >
-            <p className="portfolio__nav_text">Статичный сайт</p>
-            <img src={arrow} className="portfolio__nav_arrow" />
-          </a>
-          <a
-            href="https://botyginanatalia.github.io/russian-travel/"
-            className="portfolio__nav_link" target="_blank"
-          >
-            <p className="portfolio__nav_text">Адаптивный сайт</p>
-            <img src={arrow} className="portfolio__nav_arrow" />
-          </a>
-          <a
-            href="https://domainname.nb.nomoredomains.sbs"
-            className="portfolio__nav_link" target="_blank"
-          >
-            <p className="portfolio__nav_text">Одностраничное приложение</p>
-            <img src={arrow} className="portfolio__nav_arrow" />
-          </a>
-        </nav>
-      </div>
+      <nav className="portfolio__box">
+        <ul className="portfolio__nav">
+          <li>
+            <a
+              href="https://botyginanatalia.github.io/how-to-learn/"
+              className="portfolio__nav_link"
+              target="_blank"
+            >
+              <p className="portfolio__nav_text">Статичный сайт</p>
+              <img src={arrow} className="portfolio__nav_arrow" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://botyginanatalia.github.io/russian-travel/"
+              className="portfolio__nav_link"
+              target="_blank"
+            >
+              <p className="portfolio__nav_text">Адаптивный сайт</p>
+              <img src={arrow} className="portfolio__nav_arrow" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://domainname.nb.nomoredomains.sbs"
+              className="portfolio__nav_link"
+              target="_blank"
+            >
+              <p className="portfolio__nav_text">Одностраничное приложение</p>
+              <img src={arrow} className="portfolio__nav_arrow" />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 }
