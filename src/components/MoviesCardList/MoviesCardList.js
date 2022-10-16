f4acce80758eecb58af55be306d4aea624a23d93
Natalia <n.botygina@mail.ru>
2022-10-16 12:47:49 +0300

    frontend

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -9,61 +9,62 @@ function MoviesCardList(props) {
 
   const [displayedMovies, setDisplayedMovies] = useState([]);
 
+  const [moviesQuantity, defaultMoviesQuantity] = useState(0);
+
   const [moreMovies, showMoreMovies] = useState(0);
 
   function checkWindowWidth() {
     setWindowWidth(window.innerWidth);
   }
 
-  function changeWindowSize() {
+  useEffect(() => {
     if (windowWidth >= 1280) {
-      setDisplayedMovies(12);
+      defaultMoviesQuantity(12);
       showMoreMovies(4);
     } else if (windowWidth > 480 && windowWidth < 1280) {
-      setDisplayedMovies(8);
+      defaultMoviesQuantity(8);
       showMoreMovies(2);
     } else if (windowWidth <= 480) {
-      setDisplayedMovies(5);
+      defaultMoviesQuantity(5);
       showMoreMovies(2);
     }
-  }
-
-  function onMoreButtonClick() {
-    const films = JSON.parse(localStorage.getItem("films"));
-    setDisplayedMovies(films.slice(0, movies.length + moreMovies))
-  }
+  }, [windowWidth]);
 
   useEffect(() => {
     window.addEventListener("resize", checkWindowWidth);
-    changeWindowSize();
+    onMoreButtonClick();
   }, [windowWidth]);
+
+  useEffect(() => {
+    setDisplayedMovies(props.films && props.films.slice(0, moviesQuantity));
+  }, [moviesQuantity, props.films]);
+
+  function onMoreButtonClick() {
+    setDisplayedMovies(
+      props.films && props.films.slice(0, displayedMovies.length + moreMovies)
+    );
+  }
 
   if (props.films != null && props.films.length < 1) return <span className="moviesCardList__error">Ничего не найдено</span>
 
   return (
-    <>
     <section className="moviesCardList">
       <div className="moviesCardList__box">
         <div className="moviesCardList__element">
-        {
-          props.films != null && props.films.map(film => {
+          {displayedMovies != null && displayedMovies.map((film) => {
             return (
               <MoviesCard
                 film={film}
                 key={props.savedFilm ? film.movieId : film.id}
                 isSaved={props.isSaved}
-                savedFilm={props.savedFilm}
                 onSaveButtonClick={props.onSaveButtonClick}
                 onDeleteButtonClick={props.onDeleteButtonClick}
-                onSearchButtonClick={props.onSearchButtonClick}
               />
-            )
-          })
-        }
+            );
+          })}
         </div>
- 
         {location.pathname === "/movies" &&
-        props.films != null && props.films.length > displayedMovies.length ? (
+        props.films != null && props.films.length > displayedMovies != null && displayedMovies.length ? (
           <button
             type="button"
             className="moviesCardList__box-button"
@@ -73,11 +74,9 @@ function MoviesCardList(props) {
           </button>
         ) : (
           ""
-        )}   
-
+        )}
       </div>
     </section>
-    </>
   );
 }
 
