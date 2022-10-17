7db7cd18684eb9140336d60847f2d2186574ede4
Natalia <n.botygina@mail.ru>
2022-10-17 16:12:36 +0300

    changes after 1st review

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -3,11 +3,14 @@ import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Footer from "../Footer/Footer";
 import { MoviesApi } from "../../utils/moviesApi";
+import Preloader from "../Preloader/Preloader";
 
 function Movies(props) {
-  const [displayedMovies, setDisplayedMovies] = useState([]);  
+  const [displayedMovies, setDisplayedMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(props.loadingMovies);  
 
   function onSearchButtonClick(movieName, shortFilm) {
+    setIsLoading(true);
     MoviesApi.getOriginalMovies()
   .then((films) => {    
     const searchOptions = films.filter((movie) => movie.nameRU.toLowerCase().includes(movieName.toLowerCase()))
@@ -33,11 +36,23 @@ function Movies(props) {
     showDisplayedMovies()
   }, [props.displayedMovies])
 
+  useEffect(() => {
+    setIsLoading(props.loadingMovies);
+  }, [props.loadingMovies])
+
+  useEffect(() => {
+    if(displayedMovies){
+      setIsLoading(false);
+    }
+  }, [displayedMovies])
+
   return (
     <>
       <section className="movies">
         <SearchForm onSearchButtonClick={onSearchButtonClick} 
         defaultValue={props.defaultValue} />
+         {isLoading ?
+        <Preloader /> :
         <MoviesCardList
           films={displayedMovies}
           onMoreButtonClick={props.onMoreButtonClick}
@@ -46,6 +61,7 @@ function Movies(props) {
           onSaveButtonClick={props.onSaveButtonClick}
           onDeleteButtonClick={props.onDeleteButtonClick}
         />
+        } 
       </section>
       <Footer />
     </>
